5344654cf7b00f5a577e2f49ea2aa0b5265fc7fd
jtorresr1 <38108239+jtorresr1@users.noreply.github.com>
2020-12-29 09:52:46 -0500

    forjado
    forjado 0.9v

---
+++ Assets/2DGamekit/Scripts/reforged_sword.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class reforged_sword : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void cambiar_escena_zone4()
+    {
+        SceneManager.LoadScene("Zone4");
+    }
+}
