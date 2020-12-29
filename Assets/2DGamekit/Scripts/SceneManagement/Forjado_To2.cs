546d93c7a4e58f1a1749c7691140f4b21a65690b
jtorresr1 <38108239+jtorresr1@users.noreply.github.com>
2020-12-29 08:45:40 -0500

    Forjado
    Forja v0.5

---
+++ Assets/2DGamekit/Scripts/SceneManagement/Forjado_To2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Forjado_To2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void cambiar__escena()
+    {
+        SceneManager.LoadScene("ForjadoR2");
+    }
+}
