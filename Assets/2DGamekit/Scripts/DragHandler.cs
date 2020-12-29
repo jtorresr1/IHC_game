25642cd7e72d8cf51ac3b72718b2106d0078e8b9
Renzo Vicente Castro <38012028+renzovc987@users.noreply.github.com>
2020-12-28 20:26:02 -0500

    Combinacion
    Combinacion

---
+++ Assets/2DGamekit/Scripts/DragHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+
+
+    private void Start()
+    {
+
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+
+    }
+
+
+    private void Update()
+    {
+
+    }
+}
